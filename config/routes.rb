56fc6754b48a86caccc1ae3fc0a430afd711b212
R-D-D-D <wangrunding@gmail.com>
2019-06-16 15:52:34 +0800

    Use SSL and user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
+  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
