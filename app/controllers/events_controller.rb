2c58a23fbebb314c42242d900c43c6e7cf2cddbc
R-D-D-D <wangrunding@gmail.com>
2019-06-21 15:51:07 +0800

    add edit function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,7 +24,27 @@ class EventsController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "编辑成功!"
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+  
   def photos
 
   end
