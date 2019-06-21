7bd535ce8167fd13e561654f8127c0c8c40d8cf9
R-D-D-D <wangrunding@gmail.com>
2019-06-21 15:28:38 +0800

    fix some error

--- db/migrate/20190620075708_create_photos.rb
+++ db/migrate/20190620075708_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-
+      t.references :event, index: true
       t.timestamps
     end
   end
--- db/migrate/20190620075712_create_videos.rb
+++ db/migrate/20190620075712_create_videos.rb
@@ -1,7 +1,7 @@
 class CreateVideos < ActiveRecord::Migration[5.2]
   def change
     create_table :videos do |t|
-
+      t.references :event, index: true
       t.timestamps
     end
   end
